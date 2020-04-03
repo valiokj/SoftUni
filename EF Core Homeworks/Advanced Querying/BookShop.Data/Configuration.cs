24b5809c0907aefe0368ccf1b4d155acaf1dd7fd
Milena <mimitom14@abv.bg>
2020-04-04 02:37:18 +0300

    Advanced Querying Homework

---
+++ EF Core Homeworks/Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString => "Server=DESKTOP-PAM8VE0\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ EF Core Homeworks/Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+               // DbInitializer.ResetDatabase(db);
+            }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => new
+                {
+                    Title = b.Title
+                })
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
